4c2fb374ad64ace0e00195a47fe5cc184c62fd3d
Senali-perera <senaliperera1996@gmail.com>
2023-12-03 23:23:21 +0200

    Add high-charts and minor bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import './App.css'
+import WeatherView from "./views/WeatherView";
 
 function App() {
 
-  return (
-    <>
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-    </>
-  )
+    return (
+        <>
+            <WeatherView/>
+        </>
+    )
 }
 
 export default App
